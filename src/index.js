cd8822058cb7bbad0ec1dd5f11f5667a9c9b5d4d
Quietghost <smatyas7@googlemail.com>
2021-04-12 06:00:16 -0700

    Carosel finished

--- src/index.js
+++ src/index.js
@@ -2,35 +2,68 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Carousel } from 'antd';
+import './carosel.css';
+import { Row, Col, Carousel } from 'antd';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons'
 
 function onChange(a, b, c) {
   console.log(a, b, c);
 }
 
-const contentStyle = {
-  height: '480px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  verticalAlign: 'middle',
-  background: '#364d79',
-};
+const NextArrow = props => {
+  const { className, style, onClick } = props
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        color: 'black',
+        fontSize: '15px',
+        lineHeight: '1.5715'
+      }}
+      onClick={onClick}
+    >
+      <RightOutlined />
+    </div>
+  )
+}
+
+const PrevArrow = props => {
+  const { className, style, onClick } = props
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        color: 'black',
+        fontSize: '15px',
+        lineHeight: '1.5715'
+      }}
+      onClick={onClick}
+    >
+      <LeftOutlined />
+    </div>
+  )
+}
+
+
+const settings = {
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />
+}
+
 
 ReactDOM.render(
-  <Carousel afterChange={onChange}>
-    <div>
-    <h3 style={contentStyle}><div id="over"><img src={`${process.env.PUBLIC_URL}/assets/images/1.jpeg`}></img></div></h3>
-    </div>
-    <div>
-    <h3 style={contentStyle}><img src={`${process.env.PUBLIC_URL}/assets/images/1.jpeg`}></img></h3>
-    </div>
-    <div>
-    <h3 style={contentStyle}><img src={`${process.env.PUBLIC_URL}/assets/images/1.jpeg`}></img></h3>
-    </div>
-    <div>
-    <h3 style={contentStyle}><img src={`${process.env.PUBLIC_URL}/assets/images/1.jpeg`}></img></h3>
-    </div>
-  </Carousel>,
+  <Row justify="center">
+        <Col span={16}>
+          <Carousel dotPosition="top" afterChange={onChange} touchMove={true} arrows {...settings}> 
+            <div id="over"><img src={`${process.env.PUBLIC_URL}/assets/images/1.jpeg`} alt=""></img></div>
+            <div id="over"><img src={`${process.env.PUBLIC_URL}/assets/images/2.jpeg`} alt=""></img></div>
+          </Carousel>
+          </Col>
+          <Col span={16}>
+            <h3 class="example">Test</h3>
+          </Col>
+      </Row>,
   document.getElementById('container'),
 );
